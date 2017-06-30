442e864b0a5dc4450a3e69d021ad70e0ec17d13b
Soichiro Kato <m17se004@m.nanzan-u.ac.jp>
2017-06-30 15:07:42 +0900

    started changing screen structure

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
+"use strict";
+
 $(function() {
+  const colorList = ["red", "blue", "yellow", "green"];
   var list = [];
 
   $('#game').css('background', '#c8c8c8');
   var startButton = $('<div id="start_message">').text("click/touch me");
   startButton.prependTo('#game');
+
+  startButton.on('click', () => {
+    startButton.fadeOut(() => {
+      $('#game').css('background', '#ffffff');
+      startButton.remove();
+      showOrder(list, false);
+    });
+  });
+
+
+  function showOrder(list, again) {
+    if(!again) {
+      genList(list);
+    }
+    for(var i = 0; i < list.length; i++) {
+      console.log(colorList[list[i]]);
+    }
+  }
 });
 
 function genList(list)
